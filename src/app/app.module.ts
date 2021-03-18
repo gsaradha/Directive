6cf2380a0cb9470601e8082264b6bcaf85a6055b
gsaradha <saradha.girish@gmail.com>
2021-03-17 21:13:22 -0700

    basic functioanlities of directives

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {BasicHighlightDirective} from '../app/basic-highlight/basic-highlight.directive';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BasicHighlightDirective
   ],
   imports: [
     BrowserModule
